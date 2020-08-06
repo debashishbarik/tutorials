07d2255f54898744ab05a40f14e19e3e2dd3a01c
Barik <debashish.barik@accenture.com>
2020-08-07 01:39:57 +0530

    Initial

---
+++ project-reactor/src/test/java/com/debu/reactor/MonoTest.java
@@ -0,0 +1,13 @@
+package com.debu.reactor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class MonoTest {
+
+    @Test
+    public void test(){
+        log.info("Mono test started!!");
+    }
+}
